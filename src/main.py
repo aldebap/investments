65d30f62c5d324c3264a58d56ebcc244ce5e8127
Aldebaran Perseke <aldeba_bla@yahoo.com>
2020-07-10 00:06:17 -0300

    Argument for web server's port number included

--- src/main.py
+++ src/main.py
@@ -20,18 +20,16 @@ def main():
     #	parse command line interface arguments
     parser = argparse.ArgumentParser(description='Investments Application')
 
-    parser.add_argument('-d', '--dataFileName=', dest='dataFileName',
-                        action='store', help='specify the investments data file name')
-    parser.add_argument('--version', dest='version', action='store_true',
-                        help='output version information and exit')
+    parser.add_argument('-d', '--dataFileName=', dest='dataFileName', action='store', help='specify the investments data file name')
+    parser.add_argument('-p', '--portNumber=', default=8080, dest='portNumber', type=int, action='store', help='specify the web server\'s port number')
+    parser.add_argument('--version', dest='version', action='store_true', help='output version information and exit')
 
     args = parser.parse_args()
 
     if True == args.version:
-        sys.stdout.write('Investments Application V1.0')
-        sys.stdout.write(
-            'This is free software: you are free to change and redistribute it.')
-        sys.stdout.write('Written by Aldebaran Perseke (github.com/aldebap)')
+        sys.stdout.write('Investments Application V1.0\n')
+        sys.stdout.write('This is free software: you are free to change and redistribute it.\n')
+        sys.stdout.write('Written by Aldebaran Perseke (github.com/aldebap)\n')
         return
 
     print(">>>>> Investments Application\n\n")
@@ -41,10 +39,13 @@ def main():
         if not os.path.isfile(args.dataFileName):
             sys.stdout.write('[info] Data file will be created\n')
 
-    # TODO: use an argument to set the port number
+    if args.portNumber is not None and args.portNumber <= 0:
+        sys.stdout.write('[error] argument for --portNumber must be a positive number\n')
+        return
+
     myAPIServer = APIServer.instance()
 
-    myAPIServer.start(8080, args.dataFileName)
+    myAPIServer.start(args.portNumber, args.dataFileName)
 
 
 #   Application's entry point
